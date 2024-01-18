46b0970c0f5845b8f92726b4297a6852c839517e
Arthur Juwer Rambo <arthur.juwer99@gmail.com>
2024-01-18 18:38:21 -0300

    Aplicativo pronto e ja com seu funcionamento

--- Translate.Designer.cs
+++ Translate.Designer.cs
@@ -28,15 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.TextBox textBox1;
-            System.Windows.Forms.TextBox textBox2;
             this.panel1 = new System.Windows.Forms.Panel();
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.cmbSourceLanguage = new System.Windows.Forms.ComboBox();
+            this.cmbTargetLanguage = new System.Windows.Forms.ComboBox();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.cbSourceLanguage = new System.Windows.Forms.ComboBox();
-            this.cbTargetLanguage = new System.Windows.Forms.ComboBox();
+            this.textOutput = new System.Windows.Forms.TextBox();
             this.btnTraduzir = new System.Windows.Forms.Button();
-            textBox1 = new System.Windows.Forms.TextBox();
-            textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -44,101 +44,147 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(50)))), ((int)(((byte)(60)))));
-            this.panel1.Controls.Add(textBox1);
-            this.panel1.Location = new System.Drawing.Point(43, 130);
+            this.panel1.Controls.Add(this.textInput);
+            this.panel1.Location = new System.Drawing.Point(65, 138);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(360, 360);
+            this.panel1.Size = new System.Drawing.Size(400, 360);
             this.panel1.TabIndex = 2;
             // 
-            // textBox1
+            // textInput
             // 
-            textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(50)))), ((int)(((byte)(60)))));
-            textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            textBox1.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            textBox1.ForeColor = System.Drawing.SystemColors.Window;
-            textBox1.Location = new System.Drawing.Point(20, 20);
-            textBox1.Multiline = true;
-            textBox1.Name = "textBox1";
-            textBox1.Size = new System.Drawing.Size(320, 320);
-            textBox1.TabIndex = 2;
-            textBox1.Text = "Digite Aqui...";
+            this.textInput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(50)))), ((int)(((byte)(60)))));
+            this.textInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textInput.ForeColor = System.Drawing.Color.White;
+            this.textInput.Location = new System.Drawing.Point(20, 20);
+            this.textInput.Multiline = true;
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(360, 320);
+            this.textInput.TabIndex = 12;
+            this.textInput.Text = "Digite Aqui...";
+            // 
+            // cmbSourceLanguage
+            // 
+            this.cmbSourceLanguage.DropDownHeight = 306;
+            this.cmbSourceLanguage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbSourceLanguage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbSourceLanguage.FormattingEnabled = true;
+            this.cmbSourceLanguage.IntegralHeight = false;
+            this.cmbSourceLanguage.Location = new System.Drawing.Point(65, 109);
+            this.cmbSourceLanguage.Name = "cmbSourceLanguage";
+            this.cmbSourceLanguage.Size = new System.Drawing.Size(400, 24);
+            this.cmbSourceLanguage.TabIndex = 4;
+            // 
+            // cmbTargetLanguage
+            // 
+            this.cmbTargetLanguage.DropDownWidth = 400;
+            this.cmbTargetLanguage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbTargetLanguage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbTargetLanguage.FormattingEnabled = true;
+            this.cmbTargetLanguage.IntegralHeight = false;
+            this.cmbTargetLanguage.ItemHeight = 16;
+            this.cmbTargetLanguage.Location = new System.Drawing.Point(529, 109);
+            this.cmbTargetLanguage.Name = "cmbTargetLanguage";
+            this.cmbTargetLanguage.Size = new System.Drawing.Size(400, 24);
+            this.cmbTargetLanguage.TabIndex = 5;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(50)))), ((int)(((byte)(60)))));
-            this.panel2.Controls.Add(textBox2);
-            this.panel2.Location = new System.Drawing.Point(474, 130);
+            this.panel2.Controls.Add(this.textOutput);
+            this.panel2.Location = new System.Drawing.Point(529, 138);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(360, 360);
-            this.panel2.TabIndex = 3;
+            this.panel2.Size = new System.Drawing.Size(400, 360);
+            this.panel2.TabIndex = 9;
             // 
-            // textBox2
+            // textOutput
             // 
-            textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(50)))), ((int)(((byte)(60)))));
-            textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            textBox2.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            textBox2.ForeColor = System.Drawing.SystemColors.Window;
-            textBox2.Location = new System.Drawing.Point(20, 20);
-            textBox2.Multiline = true;
-            textBox2.Name = "textBox2";
-            textBox2.Size = new System.Drawing.Size(320, 320);
-            textBox2.TabIndex = 2;
-            textBox2.Text = "Texto Traduzido...";
-            // 
-            // cbSourceLanguage
-            // 
-            this.cbSourceLanguage.FormattingEnabled = true;
-            this.cbSourceLanguage.Location = new System.Drawing.Point(43, 515);
-            this.cbSourceLanguage.Name = "cbSourceLanguage";
-            this.cbSourceLanguage.Size = new System.Drawing.Size(360, 21);
-            this.cbSourceLanguage.TabIndex = 4;
-            // 
-            // cbTargetLanguage
-            // 
-            this.cbTargetLanguage.FormattingEnabled = true;
-            this.cbTargetLanguage.Location = new System.Drawing.Point(474, 515);
-            this.cbTargetLanguage.Name = "cbTargetLanguage";
-            this.cbTargetLanguage.Size = new System.Drawing.Size(360, 21);
-            this.cbTargetLanguage.TabIndex = 5;
+            this.textOutput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(50)))), ((int)(((byte)(60)))));
+            this.textOutput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textOutput.ForeColor = System.Drawing.Color.White;
+            this.textOutput.Location = new System.Drawing.Point(20, 20);
+            this.textOutput.Multiline = true;
+            this.textOutput.Name = "textOutput";
+            this.textOutput.Size = new System.Drawing.Size(360, 320);
+            this.textOutput.TabIndex = 10;
+            this.textOutput.Text = "Texto Traduzido...";
             // 
             // btnTraduzir
             // 
-            this.btnTraduzir.Location = new System.Drawing.Point(361, 569);
+            this.btnTraduzir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(50)))), ((int)(((byte)(55)))));
+            this.btnTraduzir.FlatAppearance.BorderSize = 0;
+            this.btnTraduzir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTraduzir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTraduzir.ForeColor = System.Drawing.Color.White;
+            this.btnTraduzir.Location = new System.Drawing.Point(407, 533);
             this.btnTraduzir.Name = "btnTraduzir";
-            this.btnTraduzir.Size = new System.Drawing.Size(161, 29);
-            this.btnTraduzir.TabIndex = 6;
+            this.btnTraduzir.Size = new System.Drawing.Size(188, 43);
+            this.btnTraduzir.TabIndex = 11;
             this.btnTraduzir.Text = "TRADUZIR";
-            this.btnTraduzir.UseVisualStyleBackColor = true;
+            this.btnTraduzir.UseVisualStyleBackColor = false;
             this.btnTraduzir.Click += new System.EventHandler(this.btnTraduzir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(290, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(437, 37);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "APLICATIVO DE TRADUZIR ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(613, 62);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "em tempo real";
             // 
             // Translate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(65)))));
-            this.ClientSize = new System.Drawing.Size(984, 661);
+            this.ClientSize = new System.Drawing.Size(984, 631);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnTraduzir);
-            this.Controls.Add(this.cbTargetLanguage);
-            this.Controls.Add(this.cbSourceLanguage);
             this.Controls.Add(this.panel2);
+            this.Controls.Add(this.cmbTargetLanguage);
+            this.Controls.Add(this.cmbSourceLanguage);
             this.Controls.Add(this.panel1);
             this.Name = "Translate";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Translate_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cmbSourceLanguage;
+        private System.Windows.Forms.ComboBox cmbTargetLanguage;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.ComboBox cbSourceLanguage;
-        private System.Windows.Forms.ComboBox cbTargetLanguage;
+        private System.Windows.Forms.TextBox textOutput;
         private System.Windows.Forms.Button btnTraduzir;
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Translate.cs
+++ Translate.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,14 +17,63 @@ namespace Translate
 {
     public partial class Translate : Form
     {
+        private const string baseUrl = "http://api.mymemory.translated.net";
+        private HttpClient httpClient;
         public Translate()
         {
             InitializeComponent();
+            httpClient= new HttpClient();  
         }
-
-        private void btnTraduzir_Click(object sender, EventArgs e)
+        private async Task<string> TranslateAsync(string text, string sourceLang, string targetLang)
         {
+            string url = $"{baseUrl}/get?q={Uri.EscapeDataString(text)}&langpair={sourceLang}|{targetLang}";
 
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            string responseJson = await response.Content.ReadAsStringAsync();
+            var translationResult = JsonConvert.DeserializeObject<TranslationReponse>(responseJson);
+
+            if (translationResult.ResponseStatus == 200)
+            {
+                return translationResult.TranslatedText;
+            }
+            return string.Empty;
+        }
+        private void Translate_Load(object sender, EventArgs e)
+        {
+            cmbSourceLanguage.Items.AddRange(new string[] {"en", "es", "fr", "de", "pt"});
+            cmbTargetLanguage.Items.AddRange(new string[] { "en", "es", "fr", "de", "pt" });
+            cmbSourceLanguage.SelectedIndex = 0;
+            cmbTargetLanguage.SelectedIndex = 1;
+        }
+        
+        public class TranslationReponse
+        {
+            [JsonProperty("responseStatus")]
+            public int ResponseStatus {  get; set; }
+            [JsonProperty("responseData")]
+            public TranslationData ResponseData { get; set; }
+            public string TranslatedText => ResponseData?.TranslatedText;
+        }
+        public class TranslationData
+        {
+            [JsonProperty("translatedText")]
+            public string TranslatedText { get; set; }
+        }
+        private async void btnTraduzir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string translatedText = await TranslateAsync(textInput.Text, cmbSourceLanguage.SelectedItem.ToString(), cmbTargetLanguage.SelectedItem.ToString());
+
+                textOutput.Text = translatedText;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro" + ex);
+                throw;
+            }
         }
     }
 }
